f74b876020cc7b299bdf5e190abc8ec07a2f62af
Thanatchon Chongmankhong <thanatchon_kus36@hotmail.com>
2024-06-01 10:24:14 +0700

    update

--- main.py
+++ main.py
@@ -47,27 +47,27 @@ def main():
     )
 
     # Debug
-    gemini_instance.docker_print(gemini_instance.today_time)
-    gemini_instance.prep_sp100_nasdaq100_dataset()
-    gemini_instance.generate_gemini_candlestick()
-    gemini_instance.docker_print(gemini_instance.today_time)
-    # Send PDF reports (minutes and summary)
-    gemini_instance.telegram_send_group_pdfs(
-        [   # List of PDF file paths to send
-            f"data/pdf/{gemini_instance.file_date}_minutes.pdf", 
-            f"data/pdf/{gemini_instance.file_date}_summary.pdf"
-        ],
-        [   # List of captions for the PDFs
-            gemini_instance.telegram_minutes_text,
-            gemini_instance.telegram_summary_text
-        ]
-    )
-
+    # gemini_instance.docker_print(gemini_instance.today_time)
+    # gemini_instance.prep_sp100_nasdaq100_dataset()
+    # gemini_instance.generate_gemini_candlestick()
+    # gemini_instance.docker_print(gemini_instance.today_time)
+    # # Send PDF reports (minutes and summary)
+    # gemini_instance.telegram_send_group_pdfs(
+    #     [   # List of PDF file paths to send
+    #         f"data/pdf/{gemini_instance.file_date}_minutes.pdf", 
+    #         f"data/pdf/{gemini_instance.file_date}_summary.pdf"
+    #     ],
+    #     [   # List of captions for the PDFs
+    #         gemini_instance.telegram_minutes_text,
+    #         gemini_instance.telegram_summary_text
+    #     ]
+    # )
     # Send generated images
-    gemini_instance.telegram_send_group_images(
-        gemini_instance.image_paths,        # List of image paths to send
-        gemini_instance.photo_caption_list   # List of captions for the images
-    )
+    # gemini_instance.telegram_send_group_images(
+    #     gemini_instance.image_paths,        # List of image paths to send
+    #     gemini_instance.photo_caption_list   # List of captions for the images
+    # )
+    # gemini_instance.docker_print(gemini_instance.today_time)
 
 
     # Wait until the next day at 00:00 before starting the main loop
